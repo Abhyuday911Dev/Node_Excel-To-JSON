004f6ed063f12e55fa5578546fc32280608249d0
Abhyuday <abhyuday911@gmail.com>
2023-04-16 03:44:04 +0530

    replacer final form 30%

--- index.js
+++ index.js
@@ -1,39 +1,13 @@
-// var express = require('express');
-// var router = express.Router();
-// const fs = require("fs");
+function replacer(key, value) {
+    const newKey = key === '_1' ? 'PART DESCRIPTION' : key;
+    return newKey === key ? value : {[newKey]: value};
+}
 
-// var Data
-
-// router.get('/', async function(req, res, next) {
-
-//   fs.readFile('./convert.json', 'utf8', async (err, data) => {
-//     if (err) throw err;
-//     counter = 0
-    
-//     Data = await JSON.parse(data);
-//     // console.log(Data)
-//     for (let key in Data) {
-//       console.log(key);
-//     }
-
-//     // Data = Data["SAPRE PARTS"]
-//     Data = JSON.stringify(Data)
-
-//     fs.writeFile('./data.json', Data, (err) => {
-//       if (err) throw err;
-//       res.json('Data written to file');
-//     });
-//   });
-  
-// });
-
-// module.exports = router;
-
-const xlsx = require('xlsx');
-const fs = require('fs');
+const xlsx = require("xlsx");
+const fs = require("fs");
 
 // Load the Excel file
-const workbook = xlsx.readFile('./input.xlsx');
+const workbook = xlsx.readFile("./input.xlsx");
 
 // Get the name of the first sheet
 const sheetName = workbook.SheetNames[1];
@@ -42,9 +16,11 @@ const sheetName = workbook.SheetNames[1];
 const sheetData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
 
 // Convert the data to JSON format
-const jsonData = JSON.stringify(sheetData, null, 2);
+
+// null is the replacer function
+const jsonData = JSON.stringify(sheetData, replacer, 2);
 
 // Write the JSON data to a file
-fs.writeFileSync('output.json', jsonData);
+fs.writeFileSync("output.json", jsonData);
 
-console.log('Conversion complete'); 
+console.log("Conversion complete");
